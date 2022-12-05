c30002f9cf21a9dbfa34563783835972c2408454
HPouzDev <119787059+HPouzDev@users.noreply.github.com>
2022-12-05 20:17:05 +0100

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,27 @@
+import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './components/login/login.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
 
+const firebaseConfig = {
+  apiKey: 'AIzaSyBYSPYR3fO_mdYncu1nOONXWQT-bWGpeMM',
+  authDomain: 'oficinaterminalbest.firebaseapp.com',
+  projectId: 'oficinaterminalbest',
+  storageBucket: 'oficinaterminalbest.appspot.com',
+  messagingSenderId: '712849466662',
+  appId: '1:712849466662:web:d6c8c850888ba68ec98de7',
+};
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    provideFirebaseApp(() => initializeApp({ firebaseConfig })),
+    provideAuth(() => getAuth()),
+  ],
   declarations: [AppComponent, LoginComponent],
   bootstrap: [AppComponent],
 })
