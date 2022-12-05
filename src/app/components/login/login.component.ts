ff0007bcd8b2a1a65a519ddfa4fd4a0eacbff8db
HPouzDev <119787059+HPouzDev@users.noreply.github.com>
2022-12-05 21:40:49 +0100

    2 files modified, 1 added

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { getAuth, signInWithEmailAndPassword } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-login',
@@ -8,4 +9,16 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
   constructor() {}
   ngOnInit() {}
+
+  sumbit(): void {
+    signInWithEmailAndPassword(this, 'proyectoappuerto@gmail.com', 'Best2022')
+      .then((userCredential) => {
+        const user = userCredential.user;
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode + ': ' + errorMessage);
+      });
+  }
 }
