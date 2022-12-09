a8dd5a5fc47da3c104640a53e420e644fa0cea27
HPouzDev <119787059+HPouzDev@users.noreply.github.com>
2022-12-09 15:02:03 +0100

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,15 +13,7 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { AppRoutingModule } from './app.routing.module';
-import { SCcontrolComponent } from './components/sccontrol/sccontrol.component';
-
-import { AsyncPipe, DecimalPipe, NgFor, NgIf } from '@angular/common';
-import { NgbdSortableHeader } from './components/sccontrolltable/sortable.directive';
-import {
-  NgbPaginationModule,
-  NgbTypeaheadModule,
-} from '@ng-bootstrap/ng-bootstrap';
-import { SccontrolltableComponent } from './components/sccontrolltable/sccontrolltable.component';
+import { TabbarComponent } from './components/tabbar/tabbar.component';
 
 @NgModule({
   imports: [
@@ -31,22 +23,13 @@ import { SccontrolltableComponent } from './components/sccontrolltable/sccontrol
     AppRoutingModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
-    NgFor,
-    DecimalPipe,
-    FormsModule,
-    AsyncPipe,
-    NgbTypeaheadModule,
-    NgbdSortableHeader,
-    NgbPaginationModule,
-    NgIf,
-    SccontrolltableComponent,
+    TabbarComponent,
   ],
   declarations: [
     AppComponent,
     LoginComponent,
     DashboardComponent,
     NavbarComponent,
-    SCcontrolComponent,
   ],
   bootstrap: [AppComponent],
 })
