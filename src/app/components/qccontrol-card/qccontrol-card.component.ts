ac430db2c17882a24f87671d1a0c72c72e634739
HPouzDev <119787059+HPouzDev@users.noreply.github.com>
2022-12-14 21:14:45 +0100

    2 files modified, 3 added

---
+++ src/app/components/qccontrol-card/qccontrol-card.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-qccontrol-card',
+  templateUrl: './qccontrol-card.component.html',
+  styleUrls: ['./qccontrol-card.component.css'],
+  standalone: true,
+})
+export class QCControlCardComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
--- src/app/components/qccontrol/qccontrol.component.ts
+++ src/app/components/qccontrol/qccontrol.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { QCControlCardComponent } from '../qccontrol-card/qccontrol-card.component';
 
 @Component({
   selector: 'app-qccontrol',
   templateUrl: './qccontrol.component.html',
   styleUrls: ['./qccontrol.component.css'],
   standalone: true,
+  imports: [QCControlCardComponent],
 })
 export class QCControlComponent implements OnInit {
   constructor() {}
