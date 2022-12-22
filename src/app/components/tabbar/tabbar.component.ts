fa120ccc507a944aa22ad9914ca06421b2b4ac8b
HPouzDev <119787059+HPouzDev@users.noreply.github.com>
2022-12-22 10:50:24 +0100

    1 binaries uploaded

--- src/app/components/tabbar/tabbar.component.ts
+++ src/app/components/tabbar/tabbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+import { Hola } from '../../../assets/img/cranePart/Hola';
 import { QCControlComponent } from '../qccontrol/qccontrol.component';
 import { SCcontrolComponent } from '../sccontrol/sccontrol.component';
 
